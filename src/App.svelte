63157264a1c44bfc6d03da555302125d6747fa74
Nikhil B <heftymausnik@gmail.com>
2022-05-28 11:32:46 +0530

    Fixed dialog (kinda), nuked unnecessary components

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,25 @@
 <script lang="ts">
-  import './App.css'
-  import Tabs from "./Tabs.svelte";
-  import "flowbite/dist/flowbite.css";
+  import Home from './Home.svelte'
+  import { faHome, faClock } from "@fortawesome/free-solid-svg-icons";
+  import { TabView, TabList, TabPanel, Tab } from "./lib/tabs.js";
+
+  import "./App.css";
 </script>
 
+<header class="w-full p-3 text-center text-2xl font-semibold border-b border-gray-400">Beans</header>
 <main>
-    <Tabs/>
+  <TabView>
+    <TabList>
+      <Tab name="Home" icon={faHome} />
+      <Tab name="Session" icon={faClock} />
+    </TabList>
+  
+    <TabPanel>
+      <Home/>
+    </TabPanel>
+  
+    <TabPanel>
+      <!-- <Session/> -->
+    </TabPanel>
+  </TabView>
 </main>
--- src/Session.svelte
+++ src/Session.svelte
@@ -1,8 +1,7 @@
 <script lang="ts">
-    import Header from "./util/Header.svelte";
 </script>
 
-<Header>Session</Header>
+<header class="text-xl fixed inset-x-0 top-0 p-3 text-center font-semibold bg-white border-b border-gray-200 pb-12">Home</header>
 <br><br><br> <!--halp-->
 <div class="flex flex-col text-3xl justify-center items-center pt-2">
 </div>
