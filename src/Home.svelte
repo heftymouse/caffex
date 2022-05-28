3cca001317f31b8fc99253320a2f05c8617fa7dc
SD <harrypotter.starwars.shrish@gmail.com>
2022-05-28 11:47:18 +0530

    Padding

--- src/Home.svelte
+++ src/Home.svelte
@@ -2,7 +2,8 @@
     let intakeDialog;
 </script>
 
-<div class="flex flex-col p-12 justify-center items-center">
+<div class="flex flex-col p-1 justify-center items-center">
+    <br><br><br>
     <button on:click={() => intakeDialog.showModal()} class="self-center h-12 text-lg bg-black hover:bg-gray-600 transition-colors align-middle leading-none text-white p-3 rounded-md">
         Add new caffeine intake
     </button>
