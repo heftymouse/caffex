bd7bed6c987fa6157d0b7f56f6dae4c62ed34072
SD <harrypotter.starwars.shrish@gmail.com>
2022-05-28 14:12:35 +0530

    Fix footer

--- src/lib/TabView.svelte
+++ src/lib/TabView.svelte
@@ -1,4 +1,4 @@
-<footer class="w-full absolute bottom-0 h-16 border-t border-gray-400 bg-white">
+<footer class="fixed w-full absolute bottom-0 h-16 border-t border-gray-400 bg-white">
     <div class="flex flex-row w-full h-full justify-center align-middle items-center space-x-6">
         <slot />
     </div>
