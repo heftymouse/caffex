9d5e42c8bffd9abfa5c7d093e96a819b12ac1449
Nikhil B <heftymausnik@gmail.com>
2022-05-29 01:52:14 +0530

    Not very good store/db syncing

--- src/stores.ts
+++ src/stores.ts
@@ -1,6 +1,14 @@
 import {writable, Writable} from "svelte/store";
 import type {CaffeineStorage} from "./lib/types";
+import { Db } from "./lib/db";
 
 export const currentTab: Writable<string> = writable("");
 export const pageName: Writable<string> = writable("Home");
 export const caffeineData: Writable<CaffeineStorage[]> = writable([]);
+// this has the caveat of making it so elements can only be appended
+caffeineData.subscribe(async (value) => {
+    let db = new Db();
+    await db.init();
+    const addedElement = value[value.length - 1];
+    db.addHistory(addedElement);
+})
